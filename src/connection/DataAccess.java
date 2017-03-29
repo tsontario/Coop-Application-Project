26b7fc1e923b28e284f2bf041d40514289e79b53
Tim Smith <tsontario@gmail.com>
2017-03-29 12:34:45 -0400

    merging

--- src/connection/DataAccess.java
+++ src/connection/DataAccess.java
@@ -20,8 +20,8 @@ public class DataAccess
             Class.forName("org.postgresql.Driver");
             //connection = DriverManager.getConnection("jdbc:postgresql://web0.site.uottawa.ca:15432/wausr027","wausr027",Password.getPass());
             //System.out.println(connection);
-            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","willieausrotas",Password.getPass());
-        }catch(Exception e){
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres","postgres",Password.getPass());
+        } catch(Exception e){
             System.out.println("No connection established: "+e.toString());
         }
     }
